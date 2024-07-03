51dda177015b780f2b48fa7302a72649ac9a893d
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-07-03 12:57:23 +0530

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import './../styles/App.css';
-import { useState, useEffect } = 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
   const [seconds, setSeconds] = useState(0);
